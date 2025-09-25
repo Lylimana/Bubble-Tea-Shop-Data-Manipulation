f597e32a17924b85be63ef68455050a2a64f599f
Miguel Manalili <manalili.mig@gmail.com>
2025-09-26 00:06:22 +0100

    Data Cleaning 16

--- DataCleaning.py
+++ DataCleaning.py
@@ -5,19 +5,19 @@ import csv
 
 
 # Importing raw bubble tea shop data.
-df1 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset1.csv")
-df2 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset2.csv")
-df3 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset3.csv")
-df4 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset4.csv")
-df5 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset5.csv")
-df6 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset6.csv")
-df7 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset7.csv")
-df8 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset8.csv")
-df9 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset9.csv")
+cleaned_data1 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset1.csv")
+cleaned_data2 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset2.csv")
+cleaned_data3 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset3.csv")
+cleaned_data4 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset4.csv")
+cleaned_data5 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset5.csv")
+cleaned_data6 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset6.csv")
+cleaned_data7 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset7.csv")
+cleaned_data8 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset8.csv")
+cleaned_data9 = pd.read_csv("LondonBubbleTeaShopsDataset/BubbleTeaShopsDataset9.csv")
 
-df_array = [df1, df2, df3, df3, df4, df5, df6, df7, df8, df9]
+cleaned_data_array = [cleaned_data1, cleaned_data2, cleaned_data3, cleaned_data3, cleaned_data4, cleaned_data5, cleaned_data6, cleaned_data7, cleaned_data8, cleaned_data9]
 
-raw_df = pd.concat(df_array)
+raw_cleaned_data = pd.concat(cleaned_data_array)
 
 # ----------------------------------------------------------------------------------------------------------------------------------
 
@@ -30,11 +30,11 @@ raw_df = pd.concat(df_array)
 # ----------------------------------------------------------------------------------------------------------------------------------
 
 # INITIAL DATA EXPLORING 
-raw_df.head(100)
+raw_cleaned_data.head(100)
 
-raw_df.shape # Output suggest 201 shops and 16 features. 
+raw_cleaned_data.shape # Output suggest 201 shops and 16 features. 
     
-raw_df.columns 
+raw_cleaned_data.columns 
     # Output:
     # 'OSrXXb', 'rllt__details', 'yi40Hd', 'RDApEe', 'rllt__details 2',
     # 'rllt__details 3', 'rllt__details 4', 'rllt__details 5', 'uDyWh',
@@ -72,12 +72,12 @@ drop_columns = [
     'rllt__details 5'
     ]
 
-raw_df.drop(drop_columns, inplace = True, axis = 1)
+raw_cleaned_data.drop(drop_columns, inplace = True, axis = 1)
 
-raw_df.columns
+raw_cleaned_data.columns
 
 # Renaming Columns 
-raw_df = raw_df.rename(columns = {
+raw_cleaned_data = raw_cleaned_data.rename(columns = {
                         'OSrXXb': 'shop_name', 
                         'rllt__details': 'type_of_store',
                         'yi40Hd': 'ratings', 
@@ -85,7 +85,7 @@ raw_df = raw_df.rename(columns = {
                         })
 
 # Checking data types
-raw_df.dtypes
+raw_cleaned_data.dtypes
     # shop_name - object 
     # type_of_store - object
     # ratings - float64
@@ -95,126 +95,126 @@ raw_df.dtypes
     # rllt_details 4 - object
 
 # Dropping null values
-raw_df = raw_df.dropna(subset ={'number_of_ratings'})
+raw_cleaned_data = raw_cleaned_data.dropna(subset ={'number_of_ratings'})
 
 # Standardization 
 
 # Removing · from 'type_of_store' Columns 
-raw_df['type_of_store'] = raw_df['type_of_store'].str.replace('·','')
+raw_cleaned_data['type_of_store'] = raw_cleaned_data['type_of_store'].str.replace('·','')
 
 # Standardizing 'number_of_ratings columns'
 # Removing '()' from 'number_of_ratings'
 
-raw_df['number_of_ratings'] = raw_df['number_of_ratings'].str.strip('()')
-    # raw_df['number_of_ratings'] = raw_df['number_of_ratings'].str.slice(1,-1) 
+raw_cleaned_data['number_of_ratings'] = raw_cleaned_data['number_of_ratings'].str.strip('()')
+    # raw_cleaned_data['number_of_ratings'] = raw_cleaned_data['number_of_ratings'].str.slice(1,-1) 
 
 
 # Changing values in 'number_of_ratings' from objects with K to int
 
     # my attempt to use regex to replace all the floats and convert to normal integers
         # mapping = {'K': '* 1e3'}
-        # raw_df['number_of_ratings'] = pd.eval(raw_df['number_of_ratings'].replace(mapping, regex = True))
+        # raw_cleaned_data['number_of_ratings'] = pd.eval(raw_cleaned_data['number_of_ratings'].replace(mapping, regex = True))
 
 
-for number_ratings in raw_df['number_of_ratings']:
+for number_ratings in raw_cleaned_data['number_of_ratings']:
     if "K" in number_ratings:
     # This can also be used: if number_ratings[-1] == "K": 
-        raw_df.loc[raw_df['number_of_ratings'] == number_ratings, 'number_of_ratings'] = number_ratings[0:-1:2]+"00"
+        raw_cleaned_data.loc[raw_cleaned_data['number_of_ratings'] == number_ratings, 'number_of_ratings'] = number_ratings[0:-1:2]+"00"
         continue
    
-raw_df['number_of_ratings'] = raw_df['number_of_ratings'].astype(int)
+raw_cleaned_data['number_of_ratings'] = raw_cleaned_data['number_of_ratings'].astype(int)
 # If I find a better solution for this, I will replace it. 
 
 
 # Splitting values in 'rllt__details' columns 
 
 # Create new columns for values to be arranged into
-    # raw_df['closing_times'] = np.nan
-    # raw_df['opening_times'] = np.nan
-    # raw_df['price'] = np.nan
-    # raw_df['phone_number'] = np.nan
-    # raw_df['location'] = np.nan
+    # raw_cleaned_data['closing_times'] = np.nan
+    # raw_cleaned_data['opening_times'] = np.nan
+    # raw_cleaned_data['price'] = np.nan
+    # raw_cleaned_data['phone_number'] = np.nan
+    # raw_cleaned_data['location'] = np.nan
     
-raw_df[['closing_times', 'opening_times','price', 'phone_number', 'location']] = np.nan
+raw_cleaned_data[['closing_times', 'opening_times','price', 'phone_number', 'location']] = np.nan
 
 # Setting Columns data type to object
-    # raw_df['closing_times'] = raw_df['closing_times'].astype(object)
-    # raw_df['opening_times'] = raw_df['opening_times'].astype(object)
-    # raw_df['price'] = raw_df['price'].astype(object)
-    # raw_df['phone_number'] = raw_df['phone_number'].astype(object)
-    # raw_df['location'] = raw_df['location'].astype(object)
+    # raw_cleaned_data['closing_times'] = raw_cleaned_data['closing_times'].astype(object)
+    # raw_cleaned_data['opening_times'] = raw_cleaned_data['opening_times'].astype(object)
+    # raw_cleaned_data['price'] = raw_cleaned_data['price'].astype(object)
+    # raw_cleaned_data['phone_number'] = raw_cleaned_data['phone_number'].astype(object)
+    # raw_cleaned_data['location'] = raw_cleaned_data['location'].astype(object)
     
-raw_df[['closing_times', 'opening_times', 'price', 'phone_number', 'location']] = raw_df[['closing_times', 'opening_times', 'price', 'phone_number', 'location']].astype(object)
+raw_cleaned_data[['closing_times', 'opening_times', 'price', 'phone_number', 'location']] = raw_cleaned_data[['closing_times', 'opening_times', 'price', 'phone_number', 'location']].astype(object)
 
 # Setting Columns data type to str to allow for loops
-    # raw_df['rllt__details 2'] = raw_df['rllt__details 2'].astype(str)
-    # raw_df['rllt__details 3'] = raw_df['rllt__details 3'].astype(str)
-    # raw_df['rllt__details 4'] = raw_df['rllt__details 4'].astype(str)
+    # raw_cleaned_data['rllt__details 2'] = raw_cleaned_data['rllt__details 2'].astype(str)
+    # raw_cleaned_data['rllt__details 3'] = raw_cleaned_data['rllt__details 3'].astype(str)
+    # raw_cleaned_data['rllt__details 4'] = raw_cleaned_data['rllt__details 4'].astype(str)
 
-raw_df[['rllt__details 2','rllt__details 3','rllt__details 4']] = raw_df[['rllt__details 2','rllt__details 3','rllt__details 4']].astype(str)
+raw_cleaned_data[['rllt__details 2','rllt__details 3','rllt__details 4']] = raw_cleaned_data[['rllt__details 2','rllt__details 3','rllt__details 4']].astype(str)
 
 # Loop to funnel values into appropriate columns for each 'rllt__details' column
-for details in raw_df['rllt__details 2']:
+for details in raw_cleaned_data['rllt__details 2']:
     if details == "null": 
         continue
     elif "Closes" in details:
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'closing_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'closing_times'] = details
     elif "Opens" in details: 
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'opening_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'opening_times'] = details
     elif "£" in details: 
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'price'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'price'] = details
     elif "07" in details:
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'phone_number'] = details
     elif "020" in details:
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'phone_number'] = details
     else:
-        raw_df.loc[raw_df['rllt__details 2'] == details, 'location'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 2'] == details, 'location'] = details
       
-for details in raw_df['rllt__details 3']:
+for details in raw_cleaned_data['rllt__details 3']:
     if details == "null": 
         continue
     elif "Closes" in details:
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'closing_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'closing_times'] = details
     elif "Opens" in details: 
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'opening_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'opening_times'] = details
     elif "£" in details: 
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'price'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'price'] = details
     elif "07" in details:
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'phone_number'] = details
     elif "020" in details:
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'phone_number'] = details
     else:
-        raw_df.loc[raw_df['rllt__details 3'] == details, 'location'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 3'] == details, 'location'] = details
         
-for details in raw_df['rllt__details 4']:
+for details in raw_cleaned_data['rllt__details 4']:
     if details == "null": 
         continue
     elif "Closes" in details:
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'closing_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'closing_times'] = details
     elif "Opens" in details: 
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'opening_times'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'opening_times'] = details
     elif "£" in details: 
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'price'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'price'] = details
     elif "07" in details:
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'phone_number'] = details
     elif "020" in details:
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'phone_number'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'phone_number'] = details
     else:
-        raw_df.loc[raw_df['rllt__details 4'] == details, 'location'] = details
+        raw_cleaned_data.loc[raw_cleaned_data['rllt__details 4'] == details, 'location'] = details
 
 # Dropping 'rllt__detail' columns 
 drop_column_details = ['rllt__details 2', 'rllt__details 3', 'rllt__details 4']
         
-raw_df.drop(drop_column_details, inplace = True, axis = 1)
+raw_cleaned_data.drop(drop_column_details, inplace = True, axis = 1)
 
 # Splitting 'closing_times' and 'opening_times' column to only have time 
-raw_df[['dot1', 'close','closing_time']] = raw_df['closing_times'].str.split(' ', n = 2, expand = True)
-raw_df[['dot2', 'open','opening_time']] = raw_df['opening_times'].str.split(' ', n = 2, expand = True)
+raw_cleaned_data[['dot1', 'close','closing_time']] = raw_cleaned_data['closing_times'].str.split(' ', n = 2, expand = True)
+raw_cleaned_data[['dot2', 'open','opening_time']] = raw_cleaned_data['opening_times'].str.split(' ', n = 2, expand = True)
 
 # Dropping unesscessary details columns 
 drop_columns_times = ['dot1', 'dot2','close','open', 'closing_times', 'opening_times']
 
-raw_df.drop(drop_columns_times, inplace = True, axis = 1)
+raw_cleaned_data.drop(drop_columns_times, inplace = True, axis = 1)
 
 # Most values in the 'price' column use £, ££ and £££ to indicate cost of product. 
 # However, there are some values that give a range of '£1-10' which doesn't align with this. 
@@ -223,9 +223,9 @@ raw_df.drop(drop_columns_times, inplace = True, axis = 1)
 # I'll be replacing values with '£1-10' with a single '£'.
 
 # Changing '£1-10' values to £ in 'price' column
-for x in raw_df['price']: 
+for x in raw_cleaned_data['price']: 
     if x == '£1–10': 
-        raw_df.loc[raw_df['price'] == x, 'price'] = '£'
+        raw_cleaned_data.loc[raw_cleaned_data['price'] == x, 'price'] = '£'
         continue
 
 
@@ -233,53 +233,53 @@ for x in raw_df['price']:
 # Setting Columns data type to str to allow for loops
 
 # Closing
-# raw_df['closing_am/pm'] = np.nan
-raw_df['closing_time'] = raw_df['closing_time'].astype(str)
+# raw_cleaned_data['closing_am/pm'] = np.nan
+raw_cleaned_data['closing_time'] = raw_cleaned_data['closing_time'].astype(str)
 
-for x in raw_df['closing_time']: 
+for x in raw_cleaned_data['closing_time']: 
     if x[-2:] == 'pm': 
-        raw_df.loc[raw_df['closing_time'] == x, 'closing_am/pm'] = "pm"
+        raw_cleaned_data.loc[raw_cleaned_data['closing_time'] == x, 'closing_am/pm'] = "pm"
     elif x[-2:] == 'am':
-        raw_df.loc[raw_df['closing_time'] == x, 'closing_am/pm'] = "am"
+        raw_cleaned_data.loc[raw_cleaned_data['closing_time'] == x, 'closing_am/pm'] = "am"
     continue
 
-for x in raw_df['closing_time']: 
+for x in raw_cleaned_data['closing_time']: 
     if x[-2:] == 'pm' or x[-2:] == 'am': 
-        raw_df.loc[raw_df['closing_time'] == x, 'closing_time'] = x[:-2]
+        raw_cleaned_data.loc[raw_cleaned_data['closing_time'] == x, 'closing_time'] = x[:-2]
     continue
 
-for x in raw_df['closing_time']: 
+for x in raw_cleaned_data['closing_time']: 
     if x != "None" and ':' not in x: 
-        raw_df.loc[raw_df['closing_time'] == x, 'closing_time'] = x + ":00"
+        raw_cleaned_data.loc[raw_cleaned_data['closing_time'] == x, 'closing_time'] = x + ":00"
     continue
 
 # Opening 
-# raw_df['opening_am/pm'] = np.nan
-raw_df['opening_time'] = raw_df['opening_time'].astype(str)
+# raw_cleaned_data['opening_am/pm'] = np.nan
+raw_cleaned_data['opening_time'] = raw_cleaned_data['opening_time'].astype(str)
 
-for x in raw_df['opening_time']: 
+for x in raw_cleaned_data['opening_time']: 
     if x[-2:] == 'pm': 
-        raw_df.loc[raw_df['opening_time'] == x, 'opening_am/pm'] = "pm"
+        raw_cleaned_data.loc[raw_cleaned_data['opening_time'] == x, 'opening_am/pm'] = "pm"
     elif x[-2:] == 'am':
-        raw_df.loc[raw_df['opening_time'] == x, 'opening_am/pm'] = "am"
+        raw_cleaned_data.loc[raw_cleaned_data['opening_time'] == x, 'opening_am/pm'] = "am"
     continue
 
-for x in raw_df['opening_time']: 
+for x in raw_cleaned_data['opening_time']: 
     if x[-2:] == 'pm' or x[-2:] == 'am': 
-        raw_df.loc[raw_df['opening_time'] == x, 'opening_time'] = x[:-2]
+        raw_cleaned_data.loc[raw_cleaned_data['opening_time'] == x, 'opening_time'] = x[:-2]
     continue
 
-for x in raw_df['opening_time']: 
+for x in raw_cleaned_data['opening_time']: 
     if x != "None" and ':' not in x: 
-        raw_df.loc[raw_df['opening_time'] == x, 'opening_time'] = x + ":00"
+        raw_cleaned_data.loc[raw_cleaned_data['opening_time'] == x, 'opening_time'] = x + ":00"
     continue
 
 # Removing · from 'phone_number' Columns 
-raw_df['phone_number'] = raw_df['phone_number'].str.replace('·','')
-raw_df['phone_number'] = raw_df['phone_number'].str.replace(' ','')
+raw_cleaned_data['phone_number'] = raw_cleaned_data['phone_number'].str.replace('·','')
+raw_cleaned_data['phone_number'] = raw_cleaned_data['phone_number'].str.replace(' ','')
 
 # Rearrange columns 
-cleaned_data = raw_df[[ 'shop_name',
+cleaned_data = raw_cleaned_data[[ 'shop_name',
                         'type_of_store',
                         'ratings',
                         'number_of_ratings',
@@ -380,9 +380,10 @@ cleaned_data = cleaned_data[cleaned_data['type_of_store'] == 'Bubble tea store']
 # Converting addresses to coordinates using geopy
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
+from geopy.extra.rate_limiter import RateLimiter
 
-# Creating columns for latitude and longitude
-cleaned_data[['latitude', 'longitude']] = np.nan
+# Creating columns for latitude, longitude and location
+cleaned_data[['latitude', 'longitude', 'location1']] = np.nan
 
 # Testing if coordinates locator works
     # geolocator = Nominatim(user_agent="boba_tea_shop_coordinate_finder")
@@ -402,21 +403,40 @@ cleaned_data = cleaned_data.drop(cleaned_data[cleaned_data['location'] == '⋅ 3
 cleaned_data = cleaned_data.drop(cleaned_data[cleaned_data['location'] == 'Temporarily closed'].index)
 
 # Finding coordinates for boba shop locations and adding it to latitude and longitude columns
-def eval_results(x):
-    try: 
-        return (x.latitude, x.longitude)
-    except: 
-        return(None, None)
+geolocator = Nominatim(user_agent = "manalili.mig@gmail.com")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds = 1)
+cleaned_data['location1'] = cleaned_data['location'].apply(geocode)
 
-for location in cleaned_data['location']:
-    if location != "nan":
-        geolocator = Nominatim(user_agent = 'manalili.mig@gmail.com')
-        cleaned_data['latitude'] = cleaned_data['location'].apply(geolocator.geocode, timeout=5).apply(lambda x: eval_results(x))
-    continue
+cleaned_data['latitude'] = cleaned_data['location1'].apply(lambda loc: loc.latitude if loc else None)
+cleaned_data['longitude'] = cleaned_data['location1'].apply(lambda loc: loc.longitude if loc else None)
 
 cleaned_data.dtypes
 cleaned_data.head(100)
 
+# Going over columns and dropping unecessary columns 
+cleaned_data.columns.tolist()
+'''
+    ['shop_name',
+    'type_of_store',
+    'ratings',
+    'number_of_ratings',
+    'price',
+    'phone_number',
+    'location',
+    'closing_time',
+    'closing_am/pm',
+    'opening_time',
+    'opening_am/pm',
+    'latitude',
+    'longitude',
+    'location1']
+'''
+
+columns_to_drop = ['type_of_store','location','closing_time','closing_am/pm','opening_time','opening_am/pm',]
+
+cleaned_data = cleaned_data.drop(columns_to_drop, axis = 1)
+
+cleaned_data.head(100)
 
 # Placing Dataset into excel file 
 # cleaned_data.to_excel('Bubble Tea Dataset.xlsx', index = False)
